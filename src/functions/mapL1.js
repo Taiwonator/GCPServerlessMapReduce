961789b40c91433784e634a6cbbdb81af5676b0c
Taiwonator <22432591+Taiwonator@users.noreply.github.com>
2022-11-28 17:19:54 +0000

    feat(map): updates function

--- src/functions/mapL1.js
+++ src/functions/mapL1.js
@@ -11,25 +11,27 @@
 
 // MAPPER L1 - READS AND SPLITS FILE FOR NEXT LAYER MAPPERS
 
-var fs = require('fs');
+exports.map = function() {
+    var fs = require('fs');
 
-try {  
-    const data = fs.readFileSync('test-book.txt', 'utf8').split(/\r?\n/); // read
-    const splitData = splitArray(data, 1); // split data
-    // read to firestore collection.
-} catch(e) {
-    console.log('Error:', e.stack);
-}
-
-function splitArray(arr, partitions) {
-    let i = 1
-    let sliceIndexes = [0]
-    while (i <= partitions) {
-        sliceIndexes.push((arr.length / partitions) * i)
-        i++
+    try {  
+        const data = fs.readFileSync('test-book.txt', 'utf8').split(/\r?\n/); // read
+        const splitData = splitArray(data, 1); // split data
+        // read to firestore collection.
+    } catch(e) {
+        console.log('Error:', e.stack);
     }
-    return sliceIndexes.map((_, i, array) => {
-        if(i === 0) return null
-        return arr.slice(array[i - 1], array[i])
-    }).filter(x => x !== null)
+
+    function splitArray(arr, partitions) {
+        let i = 1
+        let sliceIndexes = [0]
+        while (i <= partitions) {
+            sliceIndexes.push((arr.length / partitions) * i)
+            i++
+        }
+        return sliceIndexes.map((_, i, array) => {
+            if(i === 0) return null
+            return arr.slice(array[i - 1], array[i])
+        }).filter(x => x !== null)
+    }
 }
